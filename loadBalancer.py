0e1601b4c0e888cacedf0fd9bd43853ef849fc8a
Moran Rubinstein <moran14.r@gmail.com>
2019-06-08 15:02:13 +0300

    added lb server logic

--- loadBalancer.py
+++ loadBalancer.py
@@ -1,33 +1,50 @@
-import socket, SocketServer, Queue, sys, time, threading
-http_port = 80
-lock = threading.Lock()
-SERV_HOST = '10.0.0.1'
+import socket, threading, sys, time
+
+LISTENING_PORT = 80
+LOCK = threading.Lock()
+LB_SERVER_ADDRESS = '10.0.0.1'
 CLIENTS = {}
-servers = {'serv1' : {'M':2, 'P':1, 'V':1, 'time':0, 'address': '192.168.0.101', 'socket' : None},
+SERVERS = {'serv1' : {'M':2, 'P':1, 'V':1, 'time':0, 'address': '192.168.0.101', 'socket' : None},
            'serv2' : {'M':2, 'P':1, 'V':1, 'time':0, 'address': '192.168.0.102', 'socket' : None},
            'serv3' : {'M':1, 'P':2, 'V':3, 'time':0, 'address': '192.168.0.103', 'socket' : None}}
+
+
+class ClientThread(threading.Thread):
+
+    def __init__(self, client_address, client_socket):
+        threading.Thread.__init__(self)
+        self.client_socket = client_socket
+        self.client_address = client_address
+
+    def run(self):
+        request = self.client_socket.recv(2)
+        request_type = request[0]
+        request_len = request[1]
+        server_name = getBestServer(self.client_address, request_type, request_len)
+        LBPrint('recieved request {request} from {client_address}, sending to {server_address}'.format(
+                request=request,
+                client_address=self.client_address[0],
+                server_address=SERVERS[server_name]['address']))
+        server_socket = SERVERS[server_name]['socket']
+        server_socket.sendall(request)
+        data = server_socket.recv(2)
+        self.client_socket.sendall(data)
+        self.client_socket.close()
 
 def LBPrint(string):
     print '%s: %s-----' % (time.strftime('%H:%M:%S', time.localtime(time.time())), string)
 
-def getServerSocket(name):
-    return servers[name]['socket']
-
-
-def getServerAddr(name):
-    return servers[name]['address']
-
 
 def update_servers_time(current_time):
-    servers['serv1']['time'] = max(current_time, servers['serv1']['time'])
-    servers['serv2']['time'] = max(current_time, servers['serv2']['time'])
-    servers['serv3']['time'] = max(current_time, servers['serv3']['time'])
+    SERVERS['serv1']['time'] = max(current_time, SERVERS['serv1']['time'])
+    SERVERS['serv2']['time'] = max(current_time, SERVERS['serv2']['time'])
+    SERVERS['serv3']['time'] = max(current_time, SERVERS['serv3']['time'])
 
 
-def getNextServer(client_address, req_type, req_len):
-    global lock
-    lock.acquire()
-    req_len = int(req_len)
+def getBestServer(client_address, request_type, request_len):
+    global LOCK
+    LOCK.acquire()
+    request_len = int(request_len)
     if client_address in CLIENTS:
         current_time = CLIENTS[client_address]
     else: # First request of a client is in TIME=0
@@ -36,62 +53,37 @@ def getNextServer(client_address, req_type, req_len):
     update_servers_time(current_time)
 
     servers_task_time = {}
-    servers_task_time['serv1'] = servers['serv1'][req_type] * req_len
-    servers_task_time['serv2'] = servers['serv2'][req_type] * req_len
-    servers_task_time['serv3'] = servers['serv3'][req_type] * req_len
+    servers_task_time['serv1'] = SERVERS['serv1'][request_type] * request_len
+    servers_task_time['serv2'] = SERVERS['serv2'][request_type] * request_len
+    servers_task_time['serv3'] = SERVERS['serv3'][request_type] * request_len
 
-    serv1_task_end_time = servers_task_time['serv1'] + servers['serv1']['time']
-    serv2_task_end_time = servers_task_time['serv2'] + servers['serv2']['time']
-    serv3_task_end_time = servers_task_time['serv3'] + servers['serv3']['time']
+    serv1_task_end_time = servers_task_time['serv1'] + SERVERS['serv1']['time']
+    serv2_task_end_time = servers_task_time['serv2'] + SERVERS['serv2']['time']
+    serv3_task_end_time = servers_task_time['serv3'] + SERVERS['serv3']['time']
 
 
     if serv1_task_end_time < serv2_task_end_time:
         if serv1_task_end_time < serv3_task_end_time:
-            next_server = 'serv1'
+            best_server = 'serv1'
         else:
-            next_server = 'serv3'
+            best_server = 'serv3'
     elif serv2_task_end_time < serv3_task_end_time:
-        next_server = 'serv2'
+        best_server = 'serv2'
     else:
-        next_server = 'serv3'
+        best_server = 'serv3'
 
-    servers[next_server]['time'] += servers_task_time[next_server]
-    CLIENTS[client_address] = servers[next_server]['time']
+    SERVERS[best_server]['time'] += servers_task_time[best_server]
+    CLIENTS[client_address] = SERVERS[best_server]['time']
 
-    lock.release()
-    return next_server
-
-
-def parseRequest(req):
-    return (
-     req[0], req[1])
-
-
-class LoadBalancerRequestHandler(SocketServer.BaseRequestHandler):
-
-    def handle(self):
-        client_sock = self.request
-        req = client_sock.recv(2)
-        req_type, req_len = parseRequest(req)
-        server_name = getNextServer(self.client_address, req_type, req_len)
-        LBPrint('recieved request %s from %s, sending to %s' % (req, self.client_address[0], getServerAddr(server_name)))
-        serv_sock = getServerSocket(server_name)
-        serv_sock.sendall(req)
-        data = serv_sock.recv(2)
-        client_sock.sendall(data)
-        client_sock.close()
-
-
-class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    pass
-
+    LOCK.release()
+    return best_server
 
 def connectToServer(server):
     should_close_socket = False
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         should_close_socket = True
-        server_address = (server[1]['address'], http_port)
+        server_address = (server[1]['address'], LISTENING_PORT)
         sock.connect(server_address)
     except socket.error as err:
         LBPrint(err)
@@ -104,18 +96,39 @@ def connectToServer(server):
 
 def connectToAllServers():
     LBPrint('Connecting to servers')
-    for server in servers.iteritems():
+    for server in SERVERS.iteritems():
         connectToServer(server)
 
 
+def startLBServer():
+    should_close_socket = False
+    try:
+        lb_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        should_close_socket = True
+        lb_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        lb_server.bind((LB_SERVER_ADDRESS, LISTENING_PORT))
+        LBPrint("Server started")
+        LBPrint("Waiting for client request..")
+        while True:
+            lb_server.listen(5)
+            client_socket, client_address = lb_server.accept()
+            client_request_thread = ClientThread(client_address, client_socket)
+            client_request_thread.start()
+    except socket.error as err:
+        LBPrint(err)
+        LBPrint('Failed to open socket')
+        if should_close_socket:
+            lb_server.close()
+        sys.exit(1)
+
 def main():
-    LBPrint('LB Started')
+    LBPrint('LB Started2')
     try:
         connectToAllServers()
-        server = ThreadedTCPServer((SERV_HOST, http_port), LoadBalancerRequestHandler)
-        server.serve_forever()
-    except socket.error as msg:
-        LBPrint(msg)
+        startLBServer()
+    except:
+        LBPrint(sys.exc_info()[0])
 
 if __name__ == '__main__':
     main()
+
